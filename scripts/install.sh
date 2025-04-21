bbca00b4b9c4352e00bcc60fbd253f5cfca42cdb
Raj <raj.c@mirasysindia.com>
2025-04-21 11:03:14 +0530

    Bug fixes: For path, service and download commands

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,4 +1,6 @@
 # !/bin/bash
+export PATH=$HOME/.local/bin:/usr/local/cuda/bin:$PATH
+
 sudo apt-get install -y curl git git-lfs
 sudo apt-get install -y cmake build-essential zsh neovim yasm ripgrep parallel openssh-server autossh net-tools patchelf ccache
 sudo apt-get install -y libgl1 libgtk-3-dev libjpeg-dev libpng-dev libtiff-dev
@@ -7,45 +9,46 @@ sudo apt-get install -y libgstreamer1.0-dev libgstreamer-plugins-base1.0-dev lib
 sudo apt install -y qtbase5-dev qt5-qmake
 sudo apt-get install -y libyaml-cpp-dev libeigen3-dev libtool libc6 libc6-dev libusb-1.0-0 libnuma1 libnuma-dev libxdo3
 sudo apt-get install -y gcc-9 g++-9
+sudo apt install -y apt-transport-https ca-certificates curl software-properties-common
 
+# FFmpeg install with Nvidia
 cd $HOME/Downloads
 git clone https://git.videolan.org/git/ffmpeg/nv-codec-headers.git
 cd nv-codec-headers && sudo make install && cd –
-wget https://ffmpeg.org/releases/ffmpeg-7.1.1.tar.xz
-tar -xf ffmpeg-7.1.1.tar.xz && cd ffmpeg-7.1.1
+wget https://ffmpeg.org/releases/ffmpeg-7.1.1.tar.xz && tar -xf ffmpeg-7.1.1.tar.xz && cd ffmpeg-7.1.1
 ./configure --prefix=/usr/local --enable-nonfree --enable-cuda-nvcc --enable-libnpp --extra-cflags=-I/usr/local/cuda/include --extra-ldflags=-L/usr/local/cuda/lib64 --disable-static --enable-shared --enable-libx264 --enable-libx265 --enable-gpl
 make -j 12 && sudo make install
 
+# Folder and Permissions
 sudo mkdir /usr/local/mirasys && sudo mkdir /var/log/mirasys && sudo mkdir /home/mirasys
 sudo chown -R $USER:$USER /usr/local/mirasys && sudo chown -R $USER:$USER /var/log/mirasys && sudo chown $USER:$USER /home/mirasys
 mkdir /usr/local/mirasys/bin && mkdir /usr/local/mirasys/nas
 
+# Python
 sudo add-apt-repository -y ppa:deadsnakes/ppa && sudo apt-get update
 sudo apt-get install -y python3.13 python3.13-dev # python3.11-distutils
 # For PIP install
-cd ~/Downloads && wget https://bootstrap.pypa.io/get-pip.py
+# cd ~/Downloads && wget https://bootstrap.pypa.io/get-pip.py
 # python3.11 get-pip.py
 # python3.11 -m pip install --upgrade virtualenv setuptools numpy
-
 # For uv (alternative to PIP) installation
 curl -LsSf https://astral.sh/uv/install.sh | sh
-export PATH=$HOME/.local/bin:/usr/local/cuda/bin:$PATH
-
 uv venv /home/mirasys/.venv-3.13 --python python3.13
 source /home/mirasys/.venv-3.13/bin/activate
 uv pip install ultralytics lap
-uv pip install -U openvino onnx onnxruntime onnxslim requests tomli mediapipe rich
+uv pip install -U onnx onnxruntime onnxslim requests tomli rich
 uv pip install av --no-binary av
 uv pip install pyzmq scikit-image scikit-learn scipy shapely albumentations filterpy
 uv pip install git+https://github.com/openai/CLIP.git 
 uv pip install open-clip-torch
+uv pip install openvino
+uv pip install mediapipe
 
 
 #FRONTEND
-sudo apt install -y apt-transport-https ca-certificates curl software-properties-common
-curl -fsSL "https://download.docker.com/linux/ubuntu/gpg"| sudo gpg --dearmor --yes -o /etc/apt/trusted.gpg.d/docker-keyring.gpg
+wget -qO - "https://download.docker.com/linux/ubuntu/gpg"| gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/docker-keyring.gpg
 sudo chmod +r /etc/apt/trusted.gpg.d/docker-keyring.gpg
-echo "deb [arch=amd64 signed-by=/etc/apt/trusted.gpg.d/docker-keyring.gpg] https://download.docker.com/linux/ubuntu noble stable" | sudo tee -a /etc/apt/sources.list.d/docker-stable.list
+sudo sh -c 'echo "deb [arch=amd signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" > /etc/apt/sources.list.d/docker.list'
 sudo apt update
 sudo apt install docker-ce docker-ce-cli containerd.io
 sudo groupadd docker
